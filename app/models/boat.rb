e259ac78c5669b74e705a9eab1396d32587feb9c
SiamKing <thesiameseelectric@gmail.com>
2018-12-05 12:42:38 -0800

    New code

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,45 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5)
+  end
+
+  def self.dinghy
+    where("length < '20'")
+  end
+
+  def self.ship
+    where("length >= '20'")
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    self.get_boat_type('Sailboat')
+  end
+
+  def self.motorboats
+    self.get_boat_type('Motorboat')
+  end
+
+  def self.with_three_classifications
+    self.joins(:boat_classifications).group('boat_id').having('count(boat_id) > 2')
+  end
+
+  def self.longest
+    self.order(:length => :desc).limit(1)
+  end
+
+  def self.get_boat_type(boat_type)
+    self.joins(:classifications).where('classifications.name = ?', boat_type).references(:classifications)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,29 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    self.get_by_classification("Catamaran")
+  end
+
+  def self.sailors
+    self.get_by_classification("Sailboat")
+  end
+
+  def self.get_by_classification(classification)
+    self.includes(:classifications).where(classifications: {name: classification}).uniq
+  end
+
+  def self.talented_seamen
+    sailor_ids = self.sailors.pluck(:id)
+    motorboat_ids = self.get_by_classification("Motorboat").pluck(:id)
+    ids = sailor_ids & motorboat_ids
+    where(id: ids)
+  end
+
+  def self.non_sailors
+    sailor_ids = self.sailors.pluck(:id)
+    where.not(id: sailor_ids)
+  end
+
 end
